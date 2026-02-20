57ffda37c00a4506eca313d758047cb37188ce3b
Manuraj Siddhu <manurajsiddhu@gmail.com>
2026-02-20 16:45:21 +0530

    Create main_health_check.sh

---
+++ scripts/main_health_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "===== SYSTEM HEALTH CHECK =====" >> ../reports/daily_report.txt
+date >> ../reports/daily_report.txt
+
+bash cpu_monitor.sh
+bash memory_monitor.sh
+bash disk_monitor.sh
+bash service_monitor.sh
+
+echo "Health check completed" >> ../reports/daily_report.txt
+echo "---------------------------------" >> ../reports/daily_report.txt
