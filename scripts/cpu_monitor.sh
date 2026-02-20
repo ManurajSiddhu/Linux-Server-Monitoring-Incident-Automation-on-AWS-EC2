cc417134af150667969180f2018e333edb4f1be7
Manuraj Siddhu <manurajsiddhu@gmail.com>
2026-02-20 16:25:25 +0530

    Create cpu_monitor.sh

---
+++ scripts/cpu_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THRESHOLD=80
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
+
+if (( $(echo "$CPU_USAGE > $THRESHOLD" | bc -l) )); then
+  echo "$DATE | HIGH CPU USAGE: $CPU_USAGE%" >> ../logs/incident.log
+else
+  echo "$DATE | CPU OK: $CPU_USAGE%" >> ../logs/system_health.log
+fi
+
+
